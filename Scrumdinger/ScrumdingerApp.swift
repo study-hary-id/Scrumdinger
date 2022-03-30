294fd9b3e42efc9d1c14c0b9562b46a5bc046376
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-30 07:24:15 +0700

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Muhammad Haryansyah on 30/03/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
