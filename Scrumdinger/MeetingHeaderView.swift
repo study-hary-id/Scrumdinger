771bd41f13325500695643d7bab8bd59e2efd52a
study-hary-id <haryansyah.study.id@gmail.com>
2022-04-30 23:04:17 +0700

    Create MeetingHeaderView

---
+++ Scrumdinger/MeetingHeaderView.swift
@@ -0,0 +1,51 @@
+//
+//  MeetingHeaderView.swift
+//  Scrumdinger
+//
+//  Created by Muhammad Haryansyah on 30/04/22.
+//
+
+import SwiftUI
+
+struct MeetingHeaderView: View {
+    let secondsElapsed: Int
+    let secondsRemaining: Int
+
+    private var minutesRemaining: Int {
+        secondsRemaining / 60
+    }
+    private var totalSeconds: Int {
+        secondsElapsed + secondsRemaining
+    }
+    private var progress: Double {
+        guard totalSeconds > 0 else { return 1 }
+        return Double(secondsElapsed) / Double(totalSeconds)
+    }
+
+    var body: some View {
+        VStack {
+            ProgressView(value: progress)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed").font(.caption)
+                    Label("\(secondsElapsed)", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining").font(.caption)
+                    Label("\(secondsRemaining)", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+        }
+        .accessibilityElement(children: .ignore)
+        .accessibilityLabel("Time Remaining")
+        .accessibilityValue("\(minutesRemaining) minutes")
+    }
+}
+
+struct MeetingHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingHeaderView(secondsElapsed: 60, secondsRemaining: 180)
+            .previewLayout(.sizeThatFits)
+    }
+}
