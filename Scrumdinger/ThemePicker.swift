693289e25deb3fdaaada09dc250d3c8ab8142b6e
study-hary-id <haryansyah.study.id@gmail.com>
2022-04-30 21:21:46 +0700

    Create ThemePicker

---
+++ Scrumdinger/ThemePicker.swift
@@ -0,0 +1,26 @@
+//
+//  ThemePicker.swift
+//  Scrumdinger
+//
+//  Created by Muhammad Haryansyah on 30/04/22.
+//
+
+import SwiftUI
+
+struct ThemePicker: View {
+    @Binding var selection: Theme
+
+    var body: some View {
+        Picker("Theme", selection: $selection) {
+            ForEach(Theme.allCases) { theme in
+                ThemeView(theme: theme).tag(theme)
+            }
+        }
+    }
+}
+
+struct ThemePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemePicker(selection: .constant(.periwinkle))
+    }
+}
