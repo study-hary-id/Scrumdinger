82a764f199c03fc4b162bf63d06958a0865d331d
study-hary-id <haryansyah.study.id@gmail.com>
2022-04-30 23:31:26 +0700

    Add MeetingHeaderView to MeetingView

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -9,29 +9,19 @@ import SwiftUI
 
 struct MeetingView: View {
     @Binding var scrum: DailyScrum
+    @StateObject var scrumTimer = ScrumTimer()
 
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 16.0)
                 .fill(scrum.theme.mainColor)
             VStack {
-                ProgressView(value: 5, total: 15)
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text("Seconds Elapsed").font(.caption)
-                        Label("300", systemImage: "hourglass.bottomhalf.fill")
-                    }
-                    Spacer()
-                    VStack(alignment: .trailing) {
-                        Text("Seconds Remaining").font(.caption)
-                        Label("600", systemImage: "hourglass.tophalf.fill")
-                    }
-                }
-                .accessibilityElement(children: .ignore)
-                .accessibilityLabel("Time Remaining")
-                .accessibilityValue("10 minutes")
-
-                Circle().strokeBorder(lineWidth: 24)
+                MeetingHeaderView(
+                    theme: scrum.theme,
+                    secondsElapsed: scrumTimer.secondsElapsed,
+                    secondsRemaining: scrumTimer.secondsRemaining
+                )
+                Circle().strokeBorder(lineWidth: 24, antialiased: true)
                 HStack {
                     Text("Speaker 1 of 3")
                     Spacer()
